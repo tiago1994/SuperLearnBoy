0ae983dfbd0fbc9325f6e574f4d8720e0bd167b4
Tiago Matos <tiagovolta@gmail.com>
2020-05-31 20:29:55 -0300

    First levels 1 - 4

---
+++ Assets/Scripts/Moeda.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Moeda : MonoBehaviour{
+
+    private int novoLevel;
+
+    // Start is called before the first frame update
+    void Start(){
+        novoLevel = SceneManager.GetActiveScene().buildIndex + 1;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision){
+        SceneManager.LoadScene(novoLevel);
+    }
+}
